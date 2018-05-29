4e4027c26b5010b477ab261daf85bdb14dc02bfb
Kartik <kartiknarula27@gmail.com>
2018-05-29 14:47:13 +0530

    Added Passport Strategies for Customer and login/signup routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const route = require("express").Router();
 
+route.get("/user", (req, res) => res.send(req.user));
+
+// Requiring trainer's route
 route.use("/trainers", require("./trainer"));
+
+// Requiring customer's route
+route.use("/customers", require("./customer"));
 
 module.exports = route;
