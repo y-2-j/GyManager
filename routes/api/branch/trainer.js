8a91a7c4479bb52636e357d944996232ba116af8
Kartik <kartiknarula27@gmail.com>
2018-06-01 23:31:07 +0530

    Make GET route to view all trainers of a branch

--- routes/api/branch/trainer.js
+++ routes/api/branch/trainer.js
@@ -1,7 +1,7 @@
 const route = require("express").Router({ mergeParams: true });
 
 const { Branch, Trainer } = require("../../../models");
-const { checkTrainerLoggedIn } = require("../../../utils/auth");
+const { checkTrainerLoggedIn , checkBranchLoggedIn} = require("../../../utils/auth");
 
 
 // Route for a Trainer Applying to a branch
@@ -32,5 +32,35 @@ route.post("/apply", checkTrainerLoggedIn, async (req, res) => {
     }
 });
 
+// GET route to see all trainers of a branch
+route.get("/", checkBranchLoggedIn, async(req, res)=>{
+
+    try{
+        const branch = await Branch.findById(req.params.id, {
+            attributes: [],
+            include: {
+                model: Trainer,
+                attributes: { exclude: ["password"] },
+                through:{
+                    where:{
+                        status: "APPROVED"
+                    }
+                }
+            }
+        });
+        // Check if branch exists
+        if(branch===null)
+            res.status(404).send({err: "Branch not found"});
+        
+        if(req.user.id != req.params.id)
+            res.status(401).send({err: "Can't view other branch's details"});
+        
+        res.send(branch.trainers);
+
+    }catch(err){
+        console.error(err);
+        res.sendStatus(500);
+    }
+});
 
 module.exports = route;
