99690e7c7fa111357fc44f697a49324f8c95f130
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2018-06-04 22:35:25 +0530

    Update gulpfile to build only style.scss and ignore built css

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require("gulp");
 const sass = require('gulp-sass');
 
 gulp.task('sass', () => {
-    gulp.src('./src/styles/**/*.scss')
+    gulp.src('./src/styles/style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./public/css'));
 });
