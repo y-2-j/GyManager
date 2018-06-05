ad7177669c074bd1dec9b108bc4977362a7c8a17
Kartik <kartiknarula27@gmail.com>
2018-06-05 13:34:58 +0530

    Make customer and trainer login and signup page

--- public/js/common.js
+++ public/js/common.js
@@ -2,6 +2,10 @@ $(() => {
     const $menu = $(".menu");
     const $logoBtn = $(".logo-button");
     const $closeBtn = $(".menu__close-btn");
+    const $loginContainer = $(".login-container");
+    const $signupContainer = $(".signup-container");
+    const $loginBtn =$(".login-btn");
+    const $signupBtn =$(".signup-btn");
 
     $logoBtn.click(() => {
         $menu.show(200);
@@ -9,5 +13,15 @@ $(() => {
 
     $closeBtn.click(() => {
         $menu.hide(200);
+    });
+
+    $signupBtn.click(()=>{
+        $loginContainer.hide(300);
+        $signupContainer.show(300);
+    });
+
+    $loginBtn.click(()=>{
+        $signupContainer.hide(300);
+        $loginContainer.show(300);
     });
 });
