e9e2b693aa0eff0df5a8e505fda702f1dcdbd4d3
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2018-06-17 14:54:16 +0530

    Make FrontPage Layout except Testimonials Section

--- public/js/common.js
+++ public/js/common.js
@@ -2,10 +2,6 @@ $(() => {
     const $menu = $(".menu");
     const $logoBtn = $(".logo-button");
     const $closeBtn = $(".menu__close-btn");
-    const $loginContainer = $(".login-container");
-    const $signupContainer = $(".signup-container");
-    const $loginBtn =$(".login-btn");
-    const $signupBtn =$(".signup-btn");
 
     $logoBtn.click(() => {
         $menu.show(200);
@@ -13,15 +9,5 @@ $(() => {
 
     $closeBtn.click(() => {
         $menu.hide(200);
-    });
-
-    $signupBtn.click(()=>{
-        $loginContainer.hide(300);
-        $signupContainer.show(300);
-    });
-
-    $loginBtn.click(()=>{
-        $signupContainer.hide(300);
-        $loginContainer.show(300);
     });
 });
