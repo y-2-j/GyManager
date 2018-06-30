8c8af2a6235cfc99d105464874d9d9ca7e641977
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2018-06-30 21:56:56 +0530

    Merge Trainer and Customer Profile Pages into one

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,13 +1,69 @@
 $(async () => {
     const user = await fetchUser();
-    const trainer = await fetchTrainer(user.id);
 
-    trainer.branch = trainer.branches.find(({ branch_trainer }) => branch_trainer.status == "APPROVED");
+    let html = null;
+    if (user === null) {
+        window.location = "/";
+    }
+    if (user.type == "customer") {
+        const customer = await fetchCustomer(user.membershipNo);
+        html = getCustomerHtml(customer);
+    }
+    else if (user.type == "trainer") {
+        const trainer = await fetchTrainer(user.id);
 
-    const trainerHtml = getTrainerHtml(trainer);
+        trainer.branch = trainer.branches.find(({ branch_trainer }) => branch_trainer.status == "APPROVED");
+    
+        html = getTrainerHtml(trainer);
+    }
+    
     const $mainContainer = $(".main-container");
-    $mainContainer.html(trainerHtml);
+    $mainContainer.html(html);
 });
+
+const fetchCustomer = (membershipNo) => {
+    return $.get(`/api/customers/${membershipNo}`);
+};
+
+const getCustomerHtml = (customer) => {
+    return `
+        <div class="details__container">
+            <div class="details__header">
+                <img src="https://via.placeholder.com/400x400.jpg" alt="Customer Image">
+                <div class="name-container">
+                    <h2>${customer.name}</h2>
+                    <h3><b>Membership No.: </b>${customer.membershipNo}</h3>
+                </div>
+            </div>
+
+            <div class="details_content">
+                <p>
+                    <b>Preferred Time Slot: </b>${customer.preferredTime}
+                    <button>Change</button>
+                </p>
+                <p><b>Fees: </b>Rs. ${customer.fees} / month</p>
+                <p>
+                    <b>Age: </b>${customer.age}<br>
+                    <b>Weight: </b>${customer.weight} kg<br>
+                    <b>Height: </b>${customer.height} cm
+                </p>
+                </div>
+            </div>
+            <div class="sidebar">
+                <div class="branch__details">
+                    <h2>${customer.branch.name}</h2>
+                    <p><b>Member Since: </b>${customer.joinDate}</p>
+                    <p><b>Membership Expiry: </b>${customer.expiryDate}</p>
+                    <p><b>Contact: </b>${customer.branch.phoneNo}</p>   
+                </div>
+                <div class="trainer__details">
+                    <h2>${customer.trainer.name}</h2>
+                    <p>Contact: ${customer.phoneNo}</p>
+                    <p><b>Timings: </b>${customer.trainer.startTime} - ${customer.trainer.endTime}</p>
+            </div>
+        </div>    
+    `;
+};
 
 
 const fetchTrainer = (id) => {
@@ -41,12 +97,12 @@ const getTrainerHtml = (trainer) => {
             </div>
         </div>
         <div class="sidebar">
-            ${getSidebarHtml(trainer)}
+            ${getTrainerSidebarHtml(trainer)}
         </div>    
     `;
 };
 
-const getSidebarHtml = (trainer) => {
+const getTrainerSidebarHtml = (trainer) => {
     return trainer.branch ? `
         <div class="branch__details">
             <h2>${trainer.branch.name}</h2>
